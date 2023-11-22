485e28303c8472164e3e7ab69c66250ea412802e
francis waihiga <francisgwaihiga@gmail.com>
2023-11-22 22:41:22 +0000

    pycodestyle correction

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
