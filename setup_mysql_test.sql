751f04b55d159bcbe2739e369bb9b8ec4d4456cf
Kevin Warui <waruimacharia@gmail.com>
2023-11-22 07:32:03 -0800

    Edit setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,12 @@
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 USE `hbnb_test_db`;
 
---create user
+-- create user
 CREATE USER IF NOT EXISTS `hbnb_test`@`localhost`
 IDENTIFIED BY 'hbnb_test_pwd';
 
--- grant privileges
+-- grant all privileges on hbnb_test_db
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO `hbnb_test`@`localhost`;
+
+-- grant select privileges on performance_shema
 GRANT SELECT ON `performance_schema`.* TO `hbnb_test`@`localhost`;
