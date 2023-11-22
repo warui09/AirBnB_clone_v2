1500363b2d2d27350ea22023a1882b08343d40ee
francis waihiga <francisgwaihiga@gmail.com>
2023-11-22 20:27:03 +0000

    Task 3 and 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,16 @@
--- prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-USE `hbnb_test_db`;
+-- This script prepares a MySQL server for the project
+    -- A new user hbnb test is created if inexistent
+    -- Special priviledges is given to the new user
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
 
 -- create user
 CREATE USER IF NOT EXISTS `hbnb_test`@`localhost`
 IDENTIFIED BY 'hbnb_test_pwd';
 
 -- grant all privileges on hbnb_test_db
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO `hbnb_test`@`localhost`;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO `hbnb_test`@`localhost`;
 
 -- grant select privileges on performance_shema
-GRANT SELECT ON `performance_schema`.* TO `hbnb_test`@`localhost`;
+GRANT SELECT ON performance_schema.* TO `hbnb_test`@`localhost`;
