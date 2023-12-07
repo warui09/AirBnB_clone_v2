739e9e558287fec226fd07c65c092d05b083e706
Kevin Warui <waruimacharia@gmail.com>
2023-12-06 22:32:06 -0800

    Add 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of the web_static folder"""
+
+import datetime
+import os
+import tarfile
+from fabric import task
+
+output_folder = 'versions'
+output_filename = f"{output_folder}/web_static_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
+source_dir = 'web_static'
+
+@task
+def do_pack(c):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    with tarfile.open(output_filename, 'w:gz') as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+    return output_filename
