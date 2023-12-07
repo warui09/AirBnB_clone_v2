3992d2f9efc8b53f59e260d1773fc26cd75e505c
Kevin Warui <waruimacharia@gmail.com>
2023-12-07 07:28:38 -0800

    Edit 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ from fabric.api import *
 output_folder = 'versions'
 output_filename = f"{output_folder}/web_static_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
 source_dir = 'web_static'
-hosts = ['174.129.55.61', '100.26.175.71']
+env.hosts = ['174.129.55.61', '100.26.175.71']
 
 def do_pack():
     # Create output folder if it doesn't exist
